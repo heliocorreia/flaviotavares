a2467aec1e5f7a31b0bddb296559842ad6d99e18
Hélio Correia <dev@heliocorreia.org>
2013-03-10 20:39:03 -0300

    adding language menu

--- functions.php
+++ functions.php
@@ -4,5 +4,6 @@ function flaviotavares_setup() {
 	remove_action('wp_head', 'wp_generator');
 
 	register_nav_menu('main', 'Main Menu');
+	register_nav_menu('language', 'Language Menu');
 }
 add_action( 'after_setup_theme', 'flaviotavares_setup' );
