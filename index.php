f805e500311ebcd037642b31573f813aac8e638d
Hélio Correia <dev@heliocorreia.org>
2013-03-11 23:47:26 -0300

    galleria and menu

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
 	<img src="<?php echo get_stylesheet_directory_uri(); ?>/media/img/img-01c-full.jpg">
 </div>
 
-<style>
-    #galleria{ margin: 0 auto; width: 700px; height: 400px; background: #000 }
-</style>
-
 <script>
 Galleria.loadTheme('<?php echo get_stylesheet_directory_uri(); ?>/media/js/galleria/themes/classic/galleria.classic.min.js');
 Galleria.run('#galleria');
