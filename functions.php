2513d4d8af156c150a341b1f6d045b2191a3fe1b
Hélio Correia <dev@heliocorreia.org>
2013-03-17 02:20:32 -0300

    videos

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// theme setup
+
 function flaviotavares_setup() {
 	remove_action('wp_head', 'wp_generator');
 
@@ -8,11 +10,32 @@ function flaviotavares_setup() {
 }
 add_action('after_setup_theme', 'flaviotavares_setup');
 
+// filters
 
-function add_menu_title_slug_class($items) {
+remove_filter( 'the_content', 'wpautop' );
+add_filter( 'the_content', 'wpautop' , 12);
+
+function my_wp_nav_menu_objects($items) {
 	foreach ($items as $item) {
 		$item->classes[] = 'slug-' . sanitize_title($item->title);
 	}
 	return $items;    
 }
-add_filter('wp_nav_menu_objects', 'add_menu_title_slug_class');
+add_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects');
+
+function my_oembed_result($html, $url, $args) {
+	return "<div class=\"oembed-result\">$html</div>";
+}
+add_filter('oembed_result','my_oembed_result', 10, 3);
+
+// shortcodes
+
+function my_shortcode_wrapper($atts, $content = null) {
+	$content = do_shortcode($content);
+	return "<div class=\"section\">${content}</div>";
+}
+
+function my_add_shortcode(){
+   add_shortcode('div', 'my_shortcode_wrapper');
+}
+add_action( 'init', 'my_add_shortcode');
---
+++ t-videos.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: [en] Videos
+*/
+?>
+<?php get_header(); ?>
+<section id="t-videos">
+	<section class="outter">
+		<section class="inner">
+			<?php the_content(); ?>
+		</section>
+	</section>
+</section>
+<?php get_footer(); ?>
