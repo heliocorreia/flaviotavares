f25cf630f5e92c7cd172bd67e62e8eaf55f53f8c
Hélio Correia <dev@heliocorreia.org>
2013-03-17 13:10:23 -0300

    template gallery

---
+++ t-gallery.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: [en] Gallery
+*/
+?>
+<?php get_header(); ?>
+<section id="t-gallery">
+	<h1 class="main-title"><?php the_title(); ?></h1>
+</section>
+<?php get_footer(); ?>
