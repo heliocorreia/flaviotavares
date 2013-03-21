bdc9ba194f77d64017e5e94b7c357397eb68492a
Hélio Correia <dev@heliocorreia.org>
2013-03-21 00:08:39 -0300

    css contact

--- t-contact.php
+++ t-contact.php
@@ -5,25 +5,23 @@ Template Name: [en] Contact
 ?>
 <?php get_header(); ?>
 <section id="t-contact" class="vcard">
-	<div class="outter">
-		<div class="inner">
-			<address>
-				<div>
+	<div class="container">
+		<div class="outter">
+			<div class="inner">
+				<address>
 					<h1 class="fn">Ateliê Flávio Tavares</h1>
 					<p class="adr"><span class="street-address">Rua Maria José Rique, quadra 177, loteamento Cidade Recreio</span>, Altiplano, <span class="locality">João Pessoa</span> - <abbr class="region" title="Paraíba">PB</abbr> - <a href="#">Veja mapa aqui</a></p>
 					<p>Terças, quintas e sábados.</p>
 					<p>Agendar horário para visitação.</p>
 					<p class="tel">Fone: (83) 3226 7376</p>
-				</div>
-			</address>
-			<address>
-				<div>
+				</address>
+				<address>
 					<h1>Na Web</h1>
 					<p class="email">contato@flaviotavares.com.br</p>
 					<p class="url">www.facebook.com/flaviotavares</p>
 					<p class="url">www.youtube.com.br/flaviotavares</p>
-				</div>
-			</address>
+				</address>
+			</div>
 		</div>
 	</div>
 </section>
