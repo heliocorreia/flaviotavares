3ea4a32c2bb5818239a1f10d7bfc2768b8d7fb00
Hélio Correia <dev@heliocorreia.org>
2013-03-22 01:39:41 -0300

    css nav-menu/sub-menu

--- functions.php
+++ functions.php
@@ -16,10 +16,20 @@ remove_filter('the_content', 'wpautop');
 add_filter('the_content', 'wpautop' , 12);
 
 function my_wp_nav_menu_objects($items) {
+	$parents = array();
+	foreach ($items as $item) {
+		if ($item->menu_item_parent && $item->menu_item_parent > 0) {
+			$parents[] = $item->menu_item_parent;
+		}
+	}
+
 	foreach ($items as $item) {
 		$item->classes[] = 'slug-' . sanitize_title($item->title);
+		if (in_array($item->ID, $parents)) {
+			$item->classes[] = 'menu-has-submenu';
+		}
 	}
-	return $items;    
+	return $items;
 }
 add_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects');
 
