2fb5504750195237186136db81ab4adf78ac2848
Hélio Correia <dev@heliocorreia.org>
2013-04-02 00:13:23 -0300

    content from wp editor

--- t-biography.php
+++ t-biography.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Template Name: [en] Biography
+Template Name: Biografia
 */
 ?>
-<?php get_header(); ?>
+<?php get_header(); the_post(); ?>
 <section id="t-biography">
 	<div class="container">
 		<header class="content-header">
@@ -27,15 +27,7 @@ Template Name: [en] Biography
 		</header>
 		<article class="content-body">
 			<section class="content">
-				<h1>Pintando, como sempre pintou</h1>
-				<p>Nascido em João Pessoa, na Paraíba, nos anos 50, dizer que Flávio teve uma infância igual a qualquer outro garoto da época seria exagero. Filho de um pai médico com fortes tendências para o desenho, sua jornada começou ainda menino, observando os rabiscos do pai, no terraço de uma casa ampla, cheia de fruteiras e convites para as aventuras infantis.</p>
-				<p>O que era brincadeira virou coisa séria e foi na Universidade Federal da Paraíba que ele começou a profissionalizar seu traço, tendo aulas com grandes nomes da arte paraibana como, entre outros, Hermano José, que, segundo Flávio, “é das pessoas mais sábias que a Paraíba tem”.</p>
-				<p>No entanto, engana-se quem pensa que a formação do artista se deu apenas em sala de aula. Depois de ganhar um prêmio no Salão de Arte Global de Olinda, em 1974, as pinceladas de Flávio o levaram ao velho mundo, para onde nunca mais deixaria de voltar, tendo em vista os diversos admiradores que conquistou por lá. Foi desenhando, expondo e sucessivamente nessa ordem que Flávio viu sua carreira deslanchar como Artista Plástico, afinal, a melhor escola ainda é, simplesmente, produzir.</p>
-				<blockquote>
-					<p>“Quando estou pintando, estou pintando para a gente, para o povo daqui. Acho que isso valoriza a parceria que existe entre o pintor, o artista, e o povo.”</p>
-				</blockquote>
-				<p>Percebe-se, no imaginário do artista, um predomínio de figuras humanas e ele confirma que o popular é seu maior campo de interesse. Com um fabulário ligado ao folclore, ao mundo do Cordel e à literatura, diversos autores Paraibanos já passaram por seus pincéis, como Augusto dos Anjos, José Lins do Rego, Machado de Assis e José Américo de Almeida. O cinema Paraibano também exerce grande influência na obra de Flávio, que se inspira com Wladimir de Carvalho, Paulo Melo, João Ramiro e Marco Vilar.</p>
-				<p>Apesar de já ter andado por diversos cantos do mundo, o lugar desse pintor é mesmo a sua terra natal. Constantemente declara sua paixão e admiração por João Pessoa, cidade que considera extremamente afetiva. Acha que a as pessoas valorizam a arte local e, sendo assim, os artistas. Seus imensos painéis estampam construções importantes da capital paraibana, provando que sua arte é considerada patrimônio popular e de grande valia para a cultura do Estado e, consequentemente, Brasileira.</p>
+				<?php the_content(); ?>
 			</section>
 			<aside class="sidebar">
 				<ol>
