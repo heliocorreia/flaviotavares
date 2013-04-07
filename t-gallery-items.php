ffef88c081d08b4c5d757bdedeefcb09a0a4e22a
Hélio Correia <dev@heliocorreia.org>
2013-04-07 05:17:05 -0300

    bxslider/swipebox on gallery-items

--- t-gallery-items.php
+++ t-gallery-items.php
@@ -15,4 +15,27 @@ Template Name: Galeria (itens)
 		</section>
 	</div>
 </div>
+
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/media/js/swipebox/swipebox.css">
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/media/js/swipebox/jquery.swipebox.min.js"></script>
+
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/media/js/bxslider/jquery.bxslider.css">
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/media/js/bxslider/jquery.bxslider.min.js"></script>
+
+<script>
+$(document).ready(function(){
+	$gallery = $('.gallery');
+
+	$gallery.find('a').swipebox({
+		useCSS : true, // false will force the use of jQuery for animations
+		hideBarsDelay : 0 // 0 to always show caption and action bar
+	});
+
+	$gallery.bxSlider({
+		pager: false,
+		slideWidth: 'auto',
+		infiniteLoop: false
+	});
+});
+</script>
 <?php get_footer(); ?>
