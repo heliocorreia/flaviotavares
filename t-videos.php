511ed09e80942a55cfc0db4b256ecdb87f2233f2
Hélio Correia <dev@heliocorreia.org>
2013-04-11 23:59:01 -0300

    img insted of autoembed link on video

--- t-videos.php
+++ t-videos.php
@@ -4,6 +4,8 @@ Template Name: [en] Videos
 */
 ?>
 <?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/media/js/swipebox/swipebox.css">
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/media/js/swipebox/jquery.swipebox.min.js"></script>
 <section id="t-videos">
 	<div class="container">
 		<section class="outter">
@@ -13,4 +15,15 @@ Template Name: [en] Videos
 		</section>
 	</div>
 </section>
+<script>
+$('#nav-main').find('.current-menu-parent .sub-menu').prepend('<li id="nav-prev-next"><span class="prev"></span><span class="next"></span></li>');
+$(document).ready(function(){
+	$gallery = ;
+
+	$('#t-videos').find('.section a').swipebox({
+		useCSS : true, // false will force the use of jQuery for animations
+		hideBarsDelay : 0 // 0 to always show caption and action bar
+	});
+});
+</script>
 <?php get_footer(); ?>
