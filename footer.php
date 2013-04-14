7f1a52df17c380532a4e78fdfe1a0af962f47983
Hélio Correia <dev@heliocorreia.org>
2013-04-14 04:43:36 -0300

    vertically center with jquery

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
 <?php wp_footer(); ?>
+<script>
+var $submenu = $('.sub-menu');
+if ($submenu) {
+	$(window).resize(function(){
+		$submenu.css({
+		    marginTop: 0,
+			top: ($(window).height() - $submenu.outerHeight())/2
+		});
+	});
+	$(window).resize();
+}
+</script>
 </div>
 </body>
 </html>
