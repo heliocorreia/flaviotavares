0b7186b3573f84ce079137b1c1a4ba1d3e2721ba
Hélio Correia <dev@heliocorreia.org>
2013-04-21 12:37:16 -0300

    vertical align center videos template

--- t-videos.php
+++ t-videos.php
@@ -6,19 +6,11 @@ Template Name: Vídeo
 <?php get_header(); ?>
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/media/js/swipebox/swipebox.css">
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/media/js/swipebox/jquery.swipebox.min.js"></script>
-<section id="t-videos">
-	<div class="container">
-		<section class="outter">
-			<section class="inner">
-				<?php the_post(); the_content(); ?>
-			</section>
-		</section>
-	</div>
-</section>
 <script>
-$('#nav-main').find('.current-menu-parent .sub-menu').prepend('<li id="nav-prev-next"><span class="prev"></span><span class="next"></span></li>');
 $(document).ready(function(){
-	$gallery = ;
+	$('#t-videos .content').responsiveVerticalCenter({ parentSelector:'body' });
+
+	$('#nav-main').find('.current-menu-parent .sub-menu').prepend('<li id="nav-prev-next"><span class="prev"></span><span class="next"></span></li>');
 
 	$('#t-videos').find('.section a').swipebox({
 		useCSS : true, // false will force the use of jQuery for animations
@@ -26,4 +18,11 @@ $(document).ready(function(){
 	});
 });
 </script>
+<section id="t-videos">
+	<div class="container">
+		<div class="content">
+			<?php the_post(); the_content(); ?>
+		</div>
+	</div>
+</section>
 <?php get_footer(); ?>
