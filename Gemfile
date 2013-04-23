4a5e8572c6306b29880056b184750507db2ae385
Hélio Correia <dev@heliocorreia.org>
2013-04-23 11:52:43 -0300

    compass plugin compass-photoshop-drop-shadow

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'compass'
+gem 'compass-photoshop-drop-shadow'
 gem 'gettext'
 gem 'rake'
 gem 'susy'
--- libs/sass/config.rb
+++ libs/sass/config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require "compass-photoshop-drop-shadow"
 require "susy"
 
 # Set this to the root of your project when deployed:
