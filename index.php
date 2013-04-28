b5612d693e2cdf683699c6830dbb657920fd8621
Hélio Correia <dev@heliocorreia.org>
2013-04-28 06:57:53 -0300

    complete index.php js refactoring

--- index.php
+++ index.php
@@ -1,163 +1,75 @@
 <?php get_header(); ?>
 
-<div id="gallery"></div>
+<?php the_post(); the_content(); ?>
 
 <script>
 head.ready('_jquery', function(){
-	var $w = $(window),
-		$current = $('<img />'),
-		$new = $(),
-		step = 0,
-		bgList = {
-			'1024': [
-				'<?php echo get_stylesheet_directory_uri(); ?>/media/img/img-01a-full.jpg',
-				'<?php echo get_stylesheet_directory_uri(); ?>/media/img/img-01b-full.jpg',
-				'<?php echo get_stylesheet_directory_uri(); ?>/media/img/img-01c-full.jpg',
-			],
-		};
+	function getBreakpointLabel() {
+		var win_w = $(window).width(),
+			labels = ['smart', 'tablet', 'desktop', 'original'],
+			breakpoints = [320, 480, 768, 1024],
+			length = breakpoints.length,
+			selected = breakpoints[0];
 
-	var backgrounds = bgList['1024'];
-
-
-
-
-
-
-	var bgResize = function(){
-		var win_w = $w.width(),
-		win_h = $w.height();
-
-		// Load narrowest background image based on
-		// viewport width, but never load anything narrower
-		// that what's already loaded if anything.
-		var available = [
-		1024//, 1280, 1366, 1400, 1680, 1920, 2560, 3840, 4860
-		];
-
-		// if (!current || ((current < win_w) && (current < available[available.length - 1]))) {
-			var chosen = available[available.length - 1];
-			for (var i=0; i<available.length; i++) {
-				if (available[i] >= win_w) {
-					chosen = available[i];
-					break;
-				}
+		for (i=0; i<length; i++) {
+			if (breakpoints[i] >= win_w) {
+				break;
 			}
-		// }
-
-		hoff =  - horizontalOffset(win_w, $current.width());
-
-		// stretch x or y?
-		dimensions = ((win_w / win_h) < ($current.width() / $current.height()))
-			? {height: '100%', width: 'auto', marginLeft: hoff}
-			: {width: '100%', height: 'auto', marginLeft: hoff};
-
-		$new.css(dimensions);
-		$current.css(dimensions);
-	};
-	$w.resize(bgResize);
-
-	var stepIt = function(forward) {
-		var speed = 250,
-			win_w = $w.width();
-
-		if (forward) {
-			width = $current.width();
-			offset = horizontalOffset(win_w, $current.width());
-
-			$new.attr('src', backgrounds[step]).css({'left': width, 'margin-left': -offset});
-			$new.show().animate({'left': 0}, speed);
-			$current.animate({'left': -width}, {
-				duration: speed,
-				complete: function(){
-					$current.attr('src', $new.attr('src'));
-					$current.css({'left': 0 });
-					$new.hide();
-				}
-			});
-
-		} else {
-			// backward
-			width = $new.width();
-			offset = horizontalOffset(win_w, $current.width());
-
-			$new.attr('src', backgrounds[step]).css({'left': -width, 'margin-left': -offset });
-			$new.show().animate({'left': 0}, speed);
-			$current.animate({'left': width}, {
-				duration: speed,
-				complete: function(){
-					$current.attr('src', $new.attr('src'));
-					$current.css({'left': 0 });
-					$new.hide();
-				}
-			});
-		}
-	}
-
-	var stepBy = function(i){
-		step += i;
-
-		if (step < 0) {
-			step = backgrounds.length - 1;
-		}
-		if (step >= backgrounds.length || !backgrounds[step - 1] ) {
-			step = 0;
+			selected = labels[i];
 		}
 
-		stepIt((i > 0));
+		return selected
 	}
 
-	var stepPrev = function(){ stepBy(-1); }
-
-	var stepNext = function(){ stepBy(1); }
-
-	var horizontalOffset = function(win_w, img_w) {
-		offset = win_w - $current.width();
-		return (offset < 0) ? Math.abs(offset / 2) : 0;
-	}
-
-	$current.css({
-		'left': '0px',
-		'position': 'fixed',
-		'top': '0px'
-    })
-	.prependTo('body')
-	.attr('src', backgrounds[step])
-	.bind('load', function(){
-		bgResize();
+	var selectedBreakpoint = getBreakpointLabel();
+	$('img').each(function(i, el){
+		var data = $(el).data(selectedBreakpoint);
+		if (data && el.src != data) {
+			el.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+			el.src = data;
+		}
 	});
 
-	$new = $current.clone();
-	$new.hide().insertAfter($current);
+	head.ready('_bxslider', function(){
+		function calculateHOffset(outer, inner) {
+			return (inner > outer) ? (outer - inner) / 2 : 0;
+		}
 
-	var preload = function(backgrounds) {
-        var cache = [];
-        for (var i in backgrounds) {
-            var cacheImage = document.createElement('img');
-            cacheImage.src = backgrounds[i];
-            cache.push(cacheImage);
-        }
-    }
-	preload(backgrounds);
+		var $w = $(window),
+			$bxSlider,
+			$gallery = $('.gallery');
 
-	head.ready(function(){
+		$gallery.find('a').click(function(e){ e.preventDefault(); });
+
+		var windowResize = function(){
+			var w = $w.width(),
+				h = $w.height();
+
+			$gallery.find('img').each(function(i, el){
+				$el = $(el);
+				dimensions = ((w/h) > ($el.width()/$el.height()))
+					? {width: w     , height: 'auto'}
+					: {width: 'auto', height: h};
+				
+				$el.css(dimensions);
+				$el.css({marginLeft: calculateHOffset(w, $el.width())});
+			});
+			
+			$bxSlider && $bxSlider.reloadSlider();
+		};
+
+		$w.resize(windowResize).trigger('resize');
+		
+		$bxSlider = $gallery.bxSlider({
+			slideWidth: 'auto',
+			controls: false,
+			pager: false
+		});
+		
 		$('#nav-main .nav-menu').prepend('<li id="nav-prev-next"><span class="prev"></span><span class="next"></span></li>');
 		var $nav = $('#nav-prev-next');
-		$('.prev', $nav).click(stepPrev);
-		$('.next', $nav).click(stepNext);
-
-		bgResize();
-
-		// add swipe
-		$current.swipe({
-			allowPageScroll: 'auto',
-			triggerOnTouchEnd : true,
-			swipeStatus : function swipeStatus(event, phase, direction, distance, fingers) {
-				if (phase=='end' && (direction=='left' || direction=='right') ) {
-					(direction=='left') && stepNext();
-					(direction=='right') && stepPrev();
-				}
-			}
-		});
+		$('.prev', $nav).click(function() { $bxSlider.goToPrevSlide(); });
+		$('.next', $nav).click(function() { $bxSlider.goToNextSlide(); });
 	});
 });
 </script>
