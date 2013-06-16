0c8f44ded73207375223c758238e66812943bde6
Hélio Correia <dev@heliocorreia.org>
2013-06-16 03:08:08 -0300

    contact info update

--- t-contact.php
+++ t-contact.php
@@ -10,10 +10,13 @@ Template Name: Contato
 			<h1 class="main-title"><?php the_title(); ?></h1>
 			<address>
 				<h1 class="fn"><?php _e('Ateliê Flávio Tavares', 'flaviotavares'); ?></h1>
-				<p class="adr"><span class="street-address"><?php _e('Rua Maria José Rique, quadra 177, loteamento Cidade Recreio', 'flaviotavares'); ?></span>, <?php _e('Altiplano', 'flaviotavares'); ?>, <span class="locality"><?php _e('João Pessoa', 'flaviotavares'); ?></span> - <abbr class="region" title="Paraíba">PB</abbr> - <a href="#"><?php _e('Veja mapa aqui', 'flaviotavares'); ?></a></p>
-				<p><?php _e('Terças, quintas e sábados.', 'flaviotavares'); ?></p>
+				<p class="adr">
+					<span class="street-address"><?php _e('Rua Zilda Nunes da Silva. 177. Portal do Sol', 'flaviotavares'); ?></span>.<br/>
+					<?php _e('Altiplano Cabo Branco', 'flaviotavares'); ?> - <?php _e('CEP: 58046-505', 'flaviotavares'); ?>,<br/>
+					<span class="locality"><?php _e('João Pessoa', 'flaviotavares'); ?></span> - <abbr class="region" title="Paraíba">PB</abbr>
+				</p>
 				<p><?php _e('Agendar horário para visitação.', 'flaviotavares'); ?></p>
-				<p class="tel"><?php _e('Fone: (83) 3226 7376', 'flaviotavares'); ?></p>
+				<p class="tel"><?php _e('Fone: (83) 3226-5022', 'flaviotavares'); ?></p>
 			</address>
 			<address>
 				<h1><?php _e('Na Web', 'flaviotavares'); ?></h1>
