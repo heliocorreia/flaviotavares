9da337c04d288ea08f1429c7213def5cd2f50630
Hélio Correia <dev@heliocorreia.org>
2013-06-30 02:12:23 -0300

    new gallery content, without submenu

--- t-gallery.php
+++ t-gallery.php
@@ -4,16 +4,22 @@ Template Name: Galeria
 */
 ?>
 <?php get_header(); ?>
-<script>
-head.ready(function(){
-	$('.sub-menu').responsiveVerticalCenter({attribute:'top',parentSelector:'body'});
-});
-</script>
 <div id="t-gallery">
 	<div class="container">
 		<section class="content">
 			<h1 class="main-title"><?php the_title(); ?></h1>
+			<?php the_post(); the_content(); ?>
 		</section>
 	</div>
 </div>
+<script>
+head.ready('_verticalcenter', function(){
+	var $content = $('#t-gallery').find('.content');
+	$content.responsiveVerticalCenter({parentSelector:'body'});
+	$content.find('a').each(function(){
+		var $that = $(this);
+		$that.attr('rel', $that.find('img').first().attr('alt'));
+	});
+});
+</script>
 <?php get_footer(); ?>
